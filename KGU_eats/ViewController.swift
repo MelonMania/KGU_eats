4985aafe21b8b74be4b9c1c47ea24acfaf539de3
MelonMania <dnwlsskfn@gmail.com>
2021-03-26 16:44:48 +0900

    Controller Setting

---
+++ KGU_eats/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KGU_eats
+//
+//  Created by RooZin on 2021/03/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
